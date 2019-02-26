356c936ec4b6f3c3f165fba942554b4b2bb21305
Orion Riitters <orionriitters@gmail.com>
2019-02-26 02:20:46 -0600

    Added labels to generated forms.

--- js/form.js
+++ js/form.js
@@ -4,10 +4,17 @@ function initializeForm(element) {
 
     for (attr in element) {
         if (element[attr].tag) {
+            if (element[attr].label) {
+                label = document.createElement('label');
+                label.setAttribute('for', newElement);
+                label.innerHTML = element[attr].label;
+                $('#body-wrapper').append(label);
+            }
             newElement = document.createElement(element[attr].tag);
             document.createElement('br');
             $('#body-wrapper').append(newElement);
-            
+            $('#body-wrapper').append(document.createElement('br'));
+
         }
     }
 
